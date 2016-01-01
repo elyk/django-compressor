870ef87fcff8008c9039786c47809f72d499daae
elyk <kconnors51@gmail.com>
2016-01-01 12:14:15 -0500

    adding holder to look for django settings module env var

--- compressor/conf.py
+++ compressor/conf.py
@@ -81,6 +81,7 @@ class CompressorConf(AppConf):
 
     class Meta:
         prefix = 'compress'
+        holder = os.environ['DJANGO_SETTINGS_MODULE']
 
     def configure_root(self, value):
         # Uses Django's STATIC_ROOT by default
